90c57ba7287efca19d3096412479f29d11c1d574
seryafarma <enhope.estheim@gmail.com>
2023-02-15 18:39:01 +0100

    First compiling main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------------------------------------------------
 // main.cpp
 // DHT11 weather webserver.
-// Style: State Machine
+// Style: Procedural
 //---------------------------------------------------------------------------------------------------------------------
 
 //---------------------------------------------------------------------------------------------------------------------
@@ -17,6 +17,9 @@
 // Local Includes
 //---------------------------------------------------------------------------------------------------------------------
 #include "auth.hpp"
+#include "web_page.hpp"
+
+using WebPage::index_html;
 
 //---------------------------------------------------------------------------------------------------------------------
 // Defines
@@ -24,7 +27,8 @@
 #define DHT11_PIN                                                                                                      \
     14 // For ESP32 WEMOS D1 board, this is connected to the D15/SCL/D3 pin and
        // somehow it is called pin 5...
-
+// State Machine.
+#define STATE_DELAY_MS 50
 //---------------------------------------------------------------------------------------------------------------------
 // Class
 //---------------------------------------------------------------------------------------------------------------------
@@ -32,20 +36,39 @@
 //---------------------------------------------------------------------------------------------------------------------
 // Global Variables
 //---------------------------------------------------------------------------------------------------------------------
-
 DFRobot_DHT11 DHT;
 
-// current temperature & humidity, updated in loop()
-float t = 0.0;
-float h = 0.0;
+float current_temperature = 0.0;
+float current_humidity = 0.0;
 
+uint32_t previous_millis = 0;
+bool time_to_gather = false;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 //---------------------------------------------------------------------------------------------------------------------
 // Functions Declaration
 //---------------------------------------------------------------------------------------------------------------------
-
+void update_temperature();
+void update_humidity();
 //---------------------------------------------------------------------------------------------------------------------
 // Setup and Loop
 //---------------------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------------------------
+String processor(const String& var)
+{
+    Serial.println(var);
+    if (var == "TEMPERATURE")
+    {
+        return String(current_temperature);
+    }
+    else if (var == "HUMIDITY")
+    {
+        return String(current_humidity);
+    }
+    return String();
+}
 
 //---------------------------------------------------------------------------------------------------------------------
 void connect_wifi()
@@ -77,10 +100,71 @@ void setup()
     Serial.begin(9600);
     delay(1500);
     connect_wifi();
+
+    // Print ESP8266 Local IP Address
+    Serial.println(WiFi.localIP());
+
+    // Route for root / web page
+    server.on("/", HTTP_GET,
+              [](AsyncWebServerRequest* request) { request->send_P(200, "text/html", index_html, processor); });
+    server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest* request) {
+        request->send_P(200, "text/plain", String(current_temperature).c_str());
+    });
+    server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest* request) {
+        request->send_P(200, "text/plain", String(current_humidity).c_str());
+    });
+
+    // Start server
+    server.begin();
 }
 
 //---------------------------------------------------------------------------------------------------------------------
 void loop()
 {
-    // put your main code here, to run repeatedly:
+    static const uint32_t THIRTY_SECONDS = 30UL * 1000UL;
+    uint32_t current_millis = millis();
+
+    // A simple timer actually for a minute...
+    if (current_millis - previous_millis > THIRTY_SECONDS)
+    {
+        // Save the last time tick.
+        previous_millis = current_millis;
+
+        DHT.read(DHT11_PIN);
+        update_temperature();
+        update_humidity();
+    }
+    delay(STATE_DELAY_MS);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void update_temperature()
+{
+    float new_temp = DHT.temperature;
+    if (isnan(new_temp))
+    {
+        Serial.println("Failed to read from DHT sensor!");
+    }
+    else
+    {
+        current_temperature = new_temp;
+        Serial.print("Current temperature: ");
+        Serial.println(current_temperature);
+    }
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void update_humidity()
+{
+    float new_humidity = DHT.humidity;
+    if (isnan(new_humidity))
+    {
+        Serial.println("Failed to read from DHT sensor!");
+    }
+    else
+    {
+        current_humidity = new_humidity;
+        Serial.print("Current humidity: ");
+        Serial.println(current_humidity);
+    }
 }
